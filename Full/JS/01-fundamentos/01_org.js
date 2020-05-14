e888e7f52e98075336afd4c9bdaaf159eee0bb46
Ramon Abreu <53264058+ramonabreu-cdev@users.noreply.github.com>
2020-05-14 14:22:16 -0300

    Add files via upload

---
+++ Full/JS/01-fundamentos/01_org.js
@@ -0,0 +1,16 @@
+//Um cos JS é organizado em uma senteça de código
+//Ele pode vir com  ; no final ou não
+console.log("Faz teu COD");
+
+/*  JS tbm pode ser organizado em um bloco de cod
+    ou seja agrupar uma senteça de códigos
+*/ 
+
+{
+    //posso ter um bloco dentro do outro
+    {
+        console.log("Olá");
+        console.log("Mundo")
+    }
+    //A identação não tem significado na linguagem
+}
