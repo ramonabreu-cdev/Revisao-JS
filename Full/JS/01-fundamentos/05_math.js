a2fbaf51f1b276cf4b67e07e5944efe0a972e1aa
Ramon Abreu <53264058+ramonabreu-cdev@users.noreply.github.com>
2020-05-18 19:17:43 -0300

    Add files via upload

---
+++ Full/JS/01-fundamentos/05_math.js
@@ -0,0 +1,12 @@
+
+//calculo da circunferência
+
+//defini o raio
+const raio = 5.6
+//defini area com Math e pow sen a potência
+const area = Math.PI * Math.pow(raio, 2)
+
+//calculei
+console.log(area)
+//imprimi o tipo
+console.log(typeof Math)
