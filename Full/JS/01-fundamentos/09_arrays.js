e2d8d17b5aa1399d09e4376d051fd6404b681a03
ramonabreu-cdev <cdevabreu@gmail.com>
2020-05-22 21:22:46 -0300

    go

--- Full/JS/01-fundamentos/09_arrays.js
+++ Full/JS/01-fundamentos/09_arrays.js
@@ -1,4 +1,26 @@
 // estrutura linear, semelhante a um vetor
+
+//criando um Array
 const valores = [7.7, 6.6, 5.5]
+
+//imprimindo os valores em cada posição
 console.log(valores[0], valores[1])
 console.log(valores[2])
+
+//mostrar todos os elementos
+console.log(valores)
+
+//Quantidade de elementos
+console.log(valores.length)
+
+// adicionando elelmentos
+valores.push(1, 5.4, null)
+console.log(valores)
+
+//tirando o ultimo elemento do Array
+console.log(valores.pop())
+console.log(valores)
+
+//identificando o tipo
+console.log(typeof valores)
+console.log(valores)
