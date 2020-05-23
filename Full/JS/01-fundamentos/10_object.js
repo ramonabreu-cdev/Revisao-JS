5e3176eda85ce3a7fedb866ad08bb276680dbd2c
ramonabreu-cdev <cdevabreu@gmail.com>
2020-05-22 21:42:50 -0300

    go

---
+++ Full/JS/01-fundamentos/10_object.js
@@ -0,0 +1,19 @@
+//Criando o primeiro objeto
+const cell = {
+
+    }
+    //criando um nome dinamicamente
+cell.modelo = 'xiaomi note 8 '
+cell.valor = '1199.99'
+cell.cor = 'Azul'
+
+//outra notação de objeto
+const notebook = {
+    modelo: 'samsungx40',
+    valor: '2299.99',
+    cor: 'cinza chumbo '
+}
+
+//imprimindo os dois objetos
+console.log(cell)
+console.log(notebook)
