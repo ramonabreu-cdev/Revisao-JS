0e1a9816867a71008614ed58393dececeb66f443
ramonabreu-cdev <cdevabreu@gmail.com>
2020-05-23 12:01:18 -0300

    go

--- Full/JS/01-fundamentos/14_funcao02.js
+++ Full/JS/01-fundamentos/14_funcao02.js
@@ -3,10 +3,14 @@
 const imprimirSoma = function(a, b) {
     console.log(a + b)
 }
-imprimeSoma(5, 2)
+imprimirSoma(5, 2)
 
 //Armazenando uma fun arrow em uma variável
 const soma = (a, b) => {
     return a + b
 }
 console.log(soma(5, 2))
+
+//Retorno implícito
+const subtra = (a, b) => a - b
+console.log(subtra(5, 2))
