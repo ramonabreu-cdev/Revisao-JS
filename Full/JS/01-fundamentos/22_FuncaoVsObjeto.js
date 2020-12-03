50407a70d126937fcebe7f97e13a90d47fa17b77
ramonabreu-cdev <cdevabreu@gmail.com>
2020-12-03 19:38:47 -0300

    js

---
+++ Full/JS/01-fundamentos/22_FuncaoVsObjeto.js
@@ -0,0 +1,16 @@
+//Se trata de uma Função
+console.log(typeof Object);
+
+//Instânciando uma Função
+// Comparando com o Java é como se fosse Um construtor
+console.log(typeof new Object);
+
+//para um cado Cliente
+const Cliente = function() {}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
+
+//Para uma classe
+class Pruduto {}
+console.log(typeof Produto)
+console.log(typeof new Pruduto())
