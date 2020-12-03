358bf85dbef85d60e1f3d9ea341ddc5a4c8169f3
ramonabreu-cdev <cdevabreu@gmail.com>
2020-12-03 20:39:18 -0300

    move src

--- Full/JS/01-fundamentos/03_tipagemFraca.js
+++
@@ -1,3 +0,0 @@
-let qualquer = 'legal'
-console.log(qualquer)
-console.log(typeof qualquer)
---
+++ Full/JavaScript/01-fundamentos/02_vareconst.js
@@ -0,0 +1,22 @@
+
+// Usando o var para declarar variável
+
+var idade = 25
+/* 
+* var(palavra reservada) é o endereço de memória
+* idade é o identificador
+* = é o simbolo de atribuição, diferente de igualdade.
+* 25 é o valor
+*/
+
+// usando o let, como forma mais moderna e ampla
+
+let ano = 1995
+
+console.log(idade, ano)
+
+// para constantes uso a mesma estrutura
+
+const nascimento = 0
+
+console.log(nascimento)
