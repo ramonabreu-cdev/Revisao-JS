daeef96b2731aac0d6007f379943d1b5e56453e0
ramonabreu-cdev <cdevabreu@gmail.com>
2020-12-08 22:31:11 -0300

    py

---
+++ Full/Django/01_Python_Basico/01_intro.py
@@ -0,0 +1,20 @@
+# Linguagem Python
+
+#Linguagem interpretada, e De Facil leitura
+
+# Bastante Expressiva
+
+# Código Fonte(m.py) --> Byte code(m.pyc) --> Runtime (PVM)
+
+# Três modos de Execução:
+    # 1 Modo Sehll (Terminal)
+    # 2 Modo Script ( Arquivo com .py)
+    # 3 Modo Interativo ( Jupyter)
+
+# Identenção é Considerada como estrutura de Código (Sintaxe)
+    # TAB ou SPACE
+
+# comentários uso #
+ """
+ Também posso usar 3 " no fim e no começo para comentar!
+ """
