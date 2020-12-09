ae3564e9f7456c7102a7516673153393290bcf2f
ramonabreu-cdev <cdevabreu@gmail.com>
2020-12-08 23:05:53 -0300

    py

---
+++ Full/Django/01_Python_Basico/02_Numeros_eOpe.py
@@ -0,0 +1,40 @@
+"""
+OBJ : 
+    1 - Tipos de Números
+    2 - Funções e Operações
+    3 - Aritmética
+    4 - Operadores
+-------------------------------------
+1) tipos:
+    O python possui Vários tipos os Mais comuns são:
+
+    - int --> Números Interios, positivos ou Negativos: 1,2,-7
+    - float --> Números Francionários, positivos ou negativos: -7.5 , 8.4
+
+2) função:
+ type() - Para descobir qual é o tipo
+ int() ou float() para converter números
+
+ 3) Operadores :
+ Com Números:
+ --------------
+ +  -> soma  --> 2+2 = 4
+ - ->  Subtrai  -->  4-1 = 3
+ * - > Multiplica --> 2*2 = 4
+ / Divide  -->  40/2 = 20
+ ** --> Potência --> 2**2 = 4
+ % --> Módulo --> 4**2 = 16
+ int() - Converte para inteiro --> int(3.2) = 3
+ float() Converte para float --> float(4) = 4.0
+
+ Relacionais
+ -------------
+
+ == (Igualdade, equivalência)
+ != (Desigualdade)
+ > (Maior Que)
+ < (Menor Que)
+ >= (Maior que ou Igual a)
+  <= (Menor que ou Igual a)
+
+"""
---
+++ Full/Django/01_Python_Basico/03_Ex_Num_Op.py
@@ -0,0 +1,41 @@
+"""
+Vamos Agora Exemplificar Algumas Operações !
+
+"""
+
+# soma
+soma = 2+2
+print ("soma",soma)
+
+# Subtração 
+subtracao = 10-2
+print("subtracao",subtracao)
+
+#Divisão
+divisao = 10/2
+print("divisao",divisao)
+
+#Potência
+potencia = 2**2
+print("potencia",potencia)
+
+#Módulo
+modulo = 15 % 2
+print("modulo",modulo)
+
+"""
+Tipos de Dados
+"""
+
+tipoInt = type(10)
+print(tipoInt)
+
+tipofloat = type(8.4)
+print(tipofloat)
+
+a = "Sou uma String"
+print (type(a))
+
+
+
+
