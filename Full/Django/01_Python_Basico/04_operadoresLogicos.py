4266c20c61d6861bff5f0e4b0b1de72d646c4f63
ramonabreu-cdev <cdevabreu@gmail.com>
2021-01-04 23:12:38 -0300

    py

---
+++ Full/Django/01_Python_Basico/04_operadoresLogicos.py
@@ -0,0 +1,47 @@
+"""
+Operadores lógicos
+Para agrupar operações com lógica booleana, utilizaremos operadores lógicos.
+Python suporta três operadores básicos: not (não), and (e), or (ou). Esses operadores
+podem ser traduzidos como não (¬ negação), e (Λ conjunção) e ou (V disjunção).
+
+"""
+
+# Operador not
+
+"""
+>>> not True
+False
+>>> not False
+True
+"""
+
+# Operador and
+
+"""
+O operador and (e) tem sua tabela verdade representada na tabela 3.5. O operador
+and (e) resulta verdadeiro apenas quando seus dois operadores forem verdadeiros.
+
+>>> True and True
+True
+>>> True and False
+False
+>>> False and True
+False
+>>> False and False
+False
+"""
+# Operador or
+
+"""
+O  operador or (ou) é que ele resulta em falso apenas se seus dois operadores também forem falsos. Se apenas um de seus operadores for verdadeiro, ou
+se os dois forem, o resultado da operação será verdadeiro.
+
+>>> True or True
+True
+>>> True or False
+True
+>>> False or True
+True
+>>> False or False
+False
+"""
